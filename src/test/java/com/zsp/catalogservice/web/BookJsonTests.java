e1b73cb150ab73b18be0cadc0d00383103b0324f
zsp0817 <shaopeng0817@qq.com>
2025-06-08 11:53:56 +0800

    feat:chat5-jdbc-bug-fix

--- src/test/java/com/zsp/catalogservice/web/BookJsonTests.java
+++ src/test/java/com/zsp/catalogservice/web/BookJsonTests.java
@@ -38,7 +38,8 @@ public class BookJsonTests {
                     "isbn": "1234567890",
                     "title": "Title",
                     "author": "Author",
-                    "price": 9.90
+                    "price": 9.90,
+                    "publisher": "Publisher"
                 }
                 """;
         assertThat(json.parse(content))
